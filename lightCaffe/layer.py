b95e8b17db5c25fed6d582ea1b1ca783543479d0
Si Chen <sic046@ucsd.edu>
2015-05-02 15:39:03 -0700

    add data layer

--- lightCaffe/layer.py
+++ lightCaffe/layer.py
@@ -2,6 +2,8 @@ __author__ = 'sichen'
 
 import numpy as np
 import scipy as sp
+import gzip
+import cPickle
 
 
 class Layer:
@@ -20,6 +22,71 @@ class Layer:
         print "%25s: n_batch %4i, n_in %4i, n_out %4i, need_update %r" \
               % (self.name, self.n_batch, self.n_in, self.n_out, self.need_update)
         print '----------------------------------------------------------------------------------'
+
+
+class DataLayer:
+    def __init__(self, n_batch, name='Data Layer'):
+        self.name = name
+        self.n_batch = n_batch
+
+    def print_information(self):
+        print '----------------------------------------------------------------------------------'
+        print "%25s: n_batch %4i" \
+              % (self.name, self.n_batch)
+        print '----------------------------------------------------------------------------------'
+
+
+class PklDataLayer(DataLayer):
+    def __init__(self, n_batch, pkl_path, name='Pkl Data Layer'):
+        DataLayer.__init__(self, n_batch, name)
+        self.pkl_path = pkl_path
+        self.train_set_x = None
+        self.train_set_y = None
+        self.test_set_x = None
+        self.test_set_y = None
+        self.val_set_x = None
+        self.val_set_y = None
+        self.n_train_batches = 0
+        self.n_test_batches = 0
+        self.n_val_batches = 0
+        self.batch_index_train = 0
+        self.batch_index_val = 0
+        self.batch_index_test = 0
+        self.n_out = 0
+
+    def load_data(self):
+        print '... loading data'
+        f = gzip.open(self.pkl_path, 'rb')
+        train_set, val_set, test_set = cPickle.load(f)
+        f.close()
+        self.train_set_x, self.train_set_y = train_set
+        self.test_set_x, self.test_set_y = test_set
+        self.val_set_x, self.val_set_y = val_set
+        self.n_train_batches = self.train_set_x.shape[0] / self.n_batch
+        self.n_test_batches = self.test_set_x.shape[0] / self.n_batch
+        self.n_val_batches = self.val_set_x.shape[0] / self.n_batch
+        self.n_out = self.train_set_x.shape[1]
+
+    def get_next_batch_train(self):
+        batch_data = (self.train_set_x[self.batch_index_train * self.n_batch : (self.batch_index_train + 1) * self.n_batch], \
+                      self.train_set_y[self.batch_index_train * self.n_batch : (self.batch_index_train + 1) * self.n_batch])
+        self.batch_index_train += 1
+        self.batch_index_train %= self.n_train_batches
+        return batch_data
+
+    def get_next_batch_test(self):
+        batch_data = (self.test_set_x[self.batch_index_test * self.n_batch : (self.batch_index_test + 1) * self.n_batch], \
+                      self.test_set_y[self.batch_index_test * self.n_batch : (self.batch_index_test + 1) * self.n_batch])
+        self.batch_index_test += 1
+        self.batch_index_test %= self.n_test_batches
+        return batch_data
+
+    def get_next_batch_val(self):
+        batch_data = (self.val_set_x[self.batch_index_val * self.n_batch : (self.batch_index_val + 1) * self.n_batch], \
+                      self.val_set_y[self.batch_index_val * self.n_batch : (self.batch_index_val + 1) * self.n_batch])
+        self.batch_index_val += 1
+        self.batch_index_val %= self.n_val_batches
+        return batch_data
 
 
 class LossLayer(Layer):
@@ -44,8 +111,6 @@ class InnerProductLayer(Layer):
     def forward(self, btm_data):
         self.btm_data = btm_data
         btm_data_shape = self.btm_data.shape
-        assert(btm_data_shape[0] > 0)
-        assert(btm_data_shape[1] == self.n_in)
         self.top_data = np.dot(btm_data, self.W) + self.b
 
     def backward(self, top_diff):
